5491ee7dfe65e84ff684d131596d67b64ca6bff5
firmanfa <fafirmansyah20@gmail.com>
2022-09-11 19:54:47 +0700

    build: add gradle shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
     id("io.ktor.plugin") version "2.1.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.example"
